6ea605b2d63ca1af3a165ddb3389a76b0284c93f
Jerry Lee <leejb91@gmail.com>
2016-04-04 16:07:42 -0700

    add delete post and show post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,16 +1,34 @@
 var Post = require("../models/post");
+var User = require("../models/user");
 
 module.exports = {
   index:   index,
+  show:    show,
   create:  create,
   destroy: destroy
 };
 
 function index(req, res) {
-  Post.find(function(err, posts) {
+  var promise = Post.find(function(err, posts) {
     if (err) res.json({ message: "No posts" });
+    posts.forEach(function(post) {
+      User.findById(post.author, function(err, user) {
+        post.author = user.name;
+      })
+    })
+    return promise;
+  })
+  .then(function(posts) {
+    res.json({posts: posts})
+  })
+}
 
-    res.json({ posts: posts })
+function show(req, res) {
+  var id = req.params.id;
+  Post.findById(id, function(err, post) {
+    console.log("show", id)
+    if (err) res.json({ message: "Post not found because " + err });
+    res.json(post)
   })
 }
 
@@ -18,7 +36,6 @@ function create(req, res, next) {
   Post
     .create(req.body)
     .then(function(post) {
-      console.log(post);
       res.json({
         data: {
           author:     post.author,
@@ -30,13 +47,17 @@ function create(req, res, next) {
     }).catch(function(err) { next(err); });
 }
 
-function destroy() {
-  console.log("deleting post backend");
-  console.log(req.body);
+function destroy(req, res, next) {
+  var id = req.params.id;
 
-  Post
-    .delete(req.body)
-    .then(function() {
-
+  Post.remove({ _id: id }, function(err) {
+    if (err) {
+      res.send(err);
+    };
+  })
+  .then(function(post) {
+    res.json({
+      success: "yay"
     })
+  })
 }
