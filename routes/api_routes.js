129b478b6354119cedfd12a6f5b98f7afddeab29
Jerry Lee <leejb91@gmail.com>
2016-04-05 18:49:29 -0700

    update api routes with new functions

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -4,6 +4,7 @@ var express = require('express'),
 // Require controllers.
 var usersCtrl = require('../controllers/users');
 var postsCtrl = require('../controllers/posts');
+var transCtrl = require('../controllers/translations');
 
 // Require token authentication.
 var token = require('../config/token_auth');
@@ -13,7 +14,7 @@ router.post('/users',    usersCtrl.create);
 router.get( '/users/me', token.authenticate, usersCtrl.me);
 router.put( '/users/me', token.authenticate, usersCtrl.update);
 
-router.post('/token', token.create);
+router.post('/token',          token.create);
 router.post('/users/me/token', token.authenticate, token.refresh);
 
 // posts resource paths:
@@ -21,5 +22,11 @@ router.get(   '/posts',     postsCtrl.index);
 router.get(   '/posts/:id', postsCtrl.show);
 router.post(  '/posts',     postsCtrl.create);
 router.delete('/posts/:id', postsCtrl.destroy);
+
+
+// translations resource paths:
+router.get(   '/posts/:id/translations',     transCtrl.index);
+router.post(  '/posts/:id/translations',     transCtrl.create);
+router.delete('/posts/:id/translations/:id', transCtrl.destroy);
 
 module.exports = router;
