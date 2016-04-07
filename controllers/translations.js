a136f7177293efa18fe5322492fc043f1f7df27c
Jerry Lee <leejb91@gmail.com>
2016-04-07 10:50:55 -0700

    add function to delete a translation

--- controllers/translations.js
+++ controllers/translations.js
@@ -2,8 +2,8 @@ var Post = require("../models/post");
 var User = require("../models/user");
 
 module.exports = {
-  index:  index,
-  create: create,
+  index:   index,
+  create:  create,
   destroy: destroy
 };
 
@@ -21,30 +21,34 @@ function create(req, res, next) {
   console.log("creating translation in backend", req.body);
   Post.findById(req.params.id, function(err, post) {
     if (err) {
-      res.send(err)
+      res.send(err);
     } else {
       var newTranslate = {
         author:    req.body.author,
         body:      req.body.body
       };
-      console.log("post stuff before the push", post)
       post.translations.push(newTranslate);
-      console.log("post stuff", post, newTranslate)
       post.save(function(err, success) {
-        console.log("after saving", post);
-        console.log("any errors?", err)
         res.send(post);
       });
-
     }
   })
-  // .then(function(post) {
-  //   console.log("after saving", post)
-  //   res.json(post);
-  // })
-  // .catch(function(err) { next(err); });
 }
 
-function destroy() {
-
+function destroy(req, res, next) {
+  console.log("deleting translation backend");
+  Post.findById(req.body.postId, function(err, post) {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(post);
+      post.translations.id(req.params.id).remove();
+      post.save(function(err, post) {
+        if (err) {
+          res.send(err);
+        }
+        res.send(post);
+      });
+    }
+  })
 }
